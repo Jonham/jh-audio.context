ccb8e8d841285f3ba1b3685875513db6f279e9b7
Jonham.Chen <jonham@qq.com>
2015-12-05 23:18:03 +0800

    Fundamental Setting
    set up the Grunt Env

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+//  grunt.loadNpmTasks('grunt-contrib-watch');
+//  grunt.loadNpmTasks('grunt-contrib-jshint');
+  require('load-grunt-tasks')(grunt);
+  
+  grunt.initConfig({
+    jshint: {
+      js: {
+        src: 'src/js/**/*.js'
+      }
+    },
+    concat: {
+      js: {
+        files: {
+          "www/js/app.js": 'src/js/*.js'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        compress: {
+          dead_code: true,
+        }
+      },
+      compress: {
+        src: '<%= concat.js.files %>',
+        dest: '<%= concat.js.files %>'.replace('.js', '.min.js')
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          base: 'www',
+          port: 80,
+          keepalive: true,
+          open: {
+            target: "http://localhost"
+          }
+        }
+      }
+    },
+    watch: {
+      build: {
+        files: ['src/**/*.js'],
+        tasks: ['jshint:js', 'concat:js']
+      },
+      web: {
+        files: ['www/**'],
+        tasks: ['connect:server']
+      }
+    }
+  });
+  
+  grunt.registerTask('default', ['watch:build']);
+  grunt.registerTask('web', ['watch:web']);
+};
---
+++ src/js/env.js
@@ -0,0 +1,18 @@
+// Web Audio API supporting message
+//  Chrome:   10-33 with  webkit
+//            ^34         AudioContext
+//  Opera:    15-21 with  webkit
+//  Firefox:  ^25
+//  Safari:   ^6    with  webkit
+//  Edge:     ^12
+//  Mobile: -----------------------
+//  Android:  46
+//  OperaM:   33
+//  ChromeA:  46
+//  FirefoxA: 42
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+if (window.AudioContext === "undefined") {
+  console.error("Do NOT support AudioContext.");
+}
